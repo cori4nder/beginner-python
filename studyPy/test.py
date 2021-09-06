6c670cd944cd66917f02cfb91433e9e30d0b0d19
laplace1123 <lgd@academico.ufpb.br>
2021-09-06 01:11:45 -0300

    Random Topics
    Pieces of Code in Python

---
+++ studyPy/test.py
@@ -0,0 +1,3 @@
+#List
+list = ["abc", 34, True, 40, "male"]
+print(list)
---
+++ studyPy/tuple_list_lib.py
@@ -0,0 +1,21 @@
+# Tuple - hétero - estático
+mytuple = ("apple", "banana", "cherry", 25);
+print(type(mytuple));
+print(mytuple[3]);
+
+# List - homo - os valores podem ser modificados
+list = ["abc", 34, True, 40, "male"];
+list[0] = "dois";
+
+# Dic - chaves (keys) - pares
+dic = {
+  "apple": "maça",
+  "house": "casa",
+  "horse": "cavalo",
+  "number" : 52
+};
+
+
+
+
+
