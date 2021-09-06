3517b54ba76764af2cbf0a49d01900fb48bd97fb
laplace1123 <lgd@academico.ufpb.br>
2021-09-06 01:14:22 -0300

    Try&Except
    Function Try and Except +

---
+++ tryExcept/try_except1.py
@@ -0,0 +1,7 @@
+# Ao ocorrer excessões (erros), normalmente o programa irá encerrar
+# use [Try - Except] para uma rota alternativa em casa de erro
+
+try:
+  print(x) # x no defined ~ error
+except:
+  print("An exception occurred")
---
+++ tryExcept/try_except_finally.py
@@ -0,0 +1,7 @@
+try:
+  x = "foi"
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
